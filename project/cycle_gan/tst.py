4becafdfde9243dd22d32018f62d05b1cab898b6
syfrankie <51787289+syfrankie@users.noreply.github.com>
2020-12-09 00:45:34 -0600

    Add files via upload

--- project/cycle_gan/tst.py
+++ project/cycle_gan/tst.py
@@ -1,4 +1,3 @@
-import sys
 import os
 import torch
 from PIL import Image
@@ -17,7 +16,7 @@ batch_size = 1
 
 
 transforms_tst = [transforms.ToTensor(), transforms.Normalize((0.5,0.5,0.5), (0.5,0.5,0.5)) ]
-tst_set = ImageDataset('../datasets/horse2zebra/', transforms_=transforms_tst, mode='test')
+tst_set = ImageDataset('../dataset/horse2zebra/', transforms_=transforms_tst, mode='test')
 tst_loader = DataLoader(tst_set, batch_size=batch_size, shuffle=False, num_workers=8)
 
 
@@ -30,6 +29,36 @@ F.load_state_dict(torch.load('output/F.pth'))
 G.eval
 F.eval
 
+'''
+if not os.path.exists('output/X'):
+    os.makedirs('output/X')
+if not os.path.exists('output/Y'):
+    os.makedirs('output/Y')
+'''
+
+Tensor = torch.cuda.FloatTensor
+#Tensor = torch.Tensor
+
+for idx, data in enumerate(tst_loader):
+
+    X = Variable(Tensor(batch_size, 3, 256, 256).copy_(data['X']))
+    Y = Variable(Tensor(batch_size, 3, 256, 256).copy_(data['Y']))
+    G_X, F_Y = G(X), F(Y)
+    F_G_X, G_F_Y = F(G_X), G(F_Y)
+
+    origin_X = 0.5 * (X.data + 1.0)
+    origin_Y = 0.5 * (Y.data + 1.0)
+    fake_X= 0.5 * (G_X.data + 1.0)
+    fake_Y = 0.5 * (F_Y.data + 1.0)
+    cycle_X = 0.5 * (F_G_X.data + 1.0)
+    cycle_Y = 0.5 * (G_F_Y.data + 1.0)
+
+    save_image(origin_X, 'output/X/%04dorigin.png' % (idx+1))
+    save_image(origin_Y, 'output/Y/%04dorigin.png' % (idx+1))
+    save_image(fake_X, 'output/X/%04dfake.png' % (idx+1))
+    save_image(fake_Y, 'output/Y/%04dfake.png' % (idx+1))
+    save_image(cycle_X, 'output/X/%04dcycle.png' % (idx+1))
+    save_image(cycle_Y, 'output/Y/%04dcycle.png' % (idx+1))
 
 
 
